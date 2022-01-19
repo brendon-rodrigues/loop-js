22e0d1c744ce3e41ae3f0309b5a171b4882a7bb4
Brendon <rodriguesbrendon861@gmail.com>
2022-01-19 02:03:27 -0300

    subindo repositório

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log('Contagem crescente usando o for:');
+
+
+for (let i = 1; i <= 10; i++) {
+   console.log(i)
+    
+}
+
+console.log(' Contagem decrescente usando while')
+
+let num= 10
+
+while(num>0) {
+ num--;
+ console.log(num)
+ }
+
+ console.log(' Números ímpares de 1 até 100')
+
+ for(let x = 1; x <=100; x+=2){
+     console.log(x)
+ }
+
+ console.log(' Numeros pares de 0 a 100')
+
+ for(let i = 0; i <= 100; i += 2){
+    console.log(i);
+}
